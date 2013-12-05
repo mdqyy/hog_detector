bdf9614580bd717ea4af495da2d58cd26ca51c91
Nikolaos Sorros <redwood.doc.ic.ac.uk>
2013-12-05 15:26:39 +0000

    new branch. create mask debug.

--- tools/create_mask.m
+++ tools/create_mask.m
@@ -1,4 +1,4 @@
-function [mask] = create_eyes_mask(dimensions,shape)
+function [mask] = create_mask(dimensions,shape)
 
 	height = dimensions(1);
 
@@ -9,18 +9,5 @@ function [mask] = create_eyes_mask(dimensions,shape)
 
 	% calculate the convex hull for the left eye
 	mask = poly2mask( shape(:,1),shape(:,2),height,width);
-
- end
-
- function [] = old()
-
- 	% calculate the convex hull for the left eye
-	mask_lefteye = poly2mask(eyes_shape(l_eye_ind,1),eyes_shape(l_eye_ind,2),height,width);
-  	
-	% calculate the convex hull for the right eye
-  	mask_righteye = poly2mask(eyes_shape(r_eye_ind,1),eyes_shape(r_eye_ind,2),height,width);
-  	
-  	% merge the masks with and or operator 
-  	mask = mask_lefteye | mask_righteye;
 
  end
